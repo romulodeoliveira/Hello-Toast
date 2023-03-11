722890981bbb6b0f71abb6e26403abb2454271e1
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-03-11 20:33:59 -0300

    criação da lógica para contagem

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    // Variaveis usadas para a contagem
+    private int mCount = 0;
+    private TextView mShowCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // obtendo uma referencia ao TextView usando o ID definido no arquivo XML
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     // Método showToast() referente ao manipulador de cliques presente no activity_main em button_top
@@ -30,4 +38,13 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+    // Método showToast() referente ao manipulador de cliques presente no activity_main em button_top
+    // Lógica usada para o contador
+    public void countUp(View view) {
+        mCount++;
+
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
 }
